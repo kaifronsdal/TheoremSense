9c02b9dac8b415779c29d8c038050b1b3803161e
kaifronsdal <kaifronsdal@gmail.com>
2024-01-25 13:32:45 -0800

    Transform datasets

--- theoremsense/dataset.py
+++ theoremsense/dataset.py
@@ -9,15 +9,51 @@ Proofs:
 """
 
 import datasets
+from dataclasses import dataclass
+
+
+@dataclass
+class Dataset:
+    question: str
+    answer: str
+
+
+def _default_map(data):
+    return data
+
+
+@dataclass
+class HendrycksDataset(Dataset):
+    level: str
+    type: str
+
+
+def _map_hendrycks_data(data):
+    return {
+        'question': data['problem'],
+        'answer': data['answer'],
+        'level': data['level'],
+        'type': data['type']
+    }
+
+
+@dataclass
+class GSM8KDataset(Dataset):
+    pass
+
 
 BOXED_ANSWERS_DATASETS = [
     {
         'name': 'EleutherAI/hendrycks_math',
         'configs': ['algebra', 'counting_and_probability', 'geometry', 'intermediate_algebra', 'number_theory',
-                    'prealgebra', 'precalculus']
+                    'prealgebra', 'precalculus'],
+        'class': HendrycksDataset,
+        'map': _map_hendrycks_data
     }, {
         'name': 'gsm8k',
-        'configs': ['main']
+        'configs': ['main'],
+        'class': GSM8KDataset,
+        'map': _default_map
     }
 ]
 PROOFS_DATASETS = []
@@ -27,5 +63,5 @@ def load_datasets(datasets_list):
     """
     Loads datasets from huggingface datasets library
     """
-    return [datasets.load_dataset(dataset, config) for dataset in datasets_list
+    return [datasets.load_dataset(dataset['name'], config).map(dataset['map']) for dataset in datasets_list
             for config in dataset['configs']]
