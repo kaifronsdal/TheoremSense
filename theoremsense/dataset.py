4150250bf17f1a7170730139a1cf28717b62fdf2
Kai Fronsdal <kaif@turing2.stanford.edu>
2024-01-30 14:15:47 -0800

    fix dataloader

--- theoremsense/dataset.py
+++ theoremsense/dataset.py
@@ -33,15 +33,36 @@ class HendrycksDataset(BoxedDataset):
     level: str
     type: str
 
+def matching_brackets(string, idx, open_char='{', close_char='}'):
+    assert string[idx] == open_char
+    if idx < len(string):
+        opening = [i for i, c in enumerate(string[idx + 1 :]) if c == open_char]
+        closing = [i for i, c in enumerate(string[idx + 1 :]) if c == close_char]
+        for i, j in enumerate(closing):
+            if i >= len(opening) or j < opening[i]:
+                return j + idx + 1
+    return -1
 
-def _map_hendrycks_data(data):
-    return {
-        'question': data['problem'],
-        'answer': data['solution'],
-        'boxed': re.search(r'\\boxed{(.*)}', data['problem']).group(1),
-        'level': data['level'],
-        'type': data['type']
-    }
+
+def _format_hendrycks_data(dataset):
+    def map_data(data):
+        answer = data['solution'].replace('\\!', '')
+        data['solution'] = answer
+        match = re.search(r'\\boxed{', answer)
+        if match is not None:
+            boxed_start = match.end() - 1
+            boxed_end = matching_brackets(answer, boxed_start)
+            boxed = answer[boxed_start+1:boxed_end]
+        else:
+            match = re.search(r'\\boxed ', answer)
+            boxed = answer[match.end()]
+        data['boxed'] = boxed
+        return data
+
+    dataset = dataset.map(map_data)
+    dataset = dataset.rename_column('problem', 'question')
+    dataset = dataset.rename_column('solution', 'answer')
+    return dataset
 
 
 @dataclass
@@ -49,12 +70,11 @@ class GSM8KDataset(BoxedDataset):
     pass
 
 
-def _map_gsm8k_data(data):
-    return {
-        'question': data['question'],
-        'answer': data['answer'],
-        'boxed': re.search(r'\\n#### (.*)', data['question']).group(1)
-    }
+def _format_gsm8k_data(dataset):
+    def map_data(data):
+        data['boxed'] = re.search(r'\n#### (.*)', data['answer']).group(1)
+        return data
+    return dataset.map(map_data)
 
 
 BOXED_ANSWERS_DATASETS = [
@@ -63,12 +83,12 @@ BOXED_ANSWERS_DATASETS = [
         'configs': ['algebra', 'counting_and_probability', 'geometry', 'intermediate_algebra', 'number_theory',
                     'prealgebra', 'precalculus'],
         'class': HendrycksDataset,
-        'map': _map_hendrycks_data
+        'format': _format_hendrycks_data
     }, {
         'name': 'gsm8k',
         'configs': ['main'],
         'class': GSM8KDataset,
-        'map': _map_gsm8k_data
+        'format': _format_gsm8k_data
     }
 ]
 PROOFS_DATASETS = []
@@ -78,5 +98,10 @@ def load_datasets(datasets_list):
     """
     Loads datasets from huggingface datasets library
     """
-    return [datasets.load_dataset(dataset['name'], config).map(dataset['map']) for dataset in datasets_list
-            for config in dataset['configs']]
+    return [
+        dataset['format'](
+            datasets.load_dataset(dataset['name'], config, download_mode='force_redownload')
+        )
+        for dataset in datasets_list
+        for config in dataset['configs']
+    ]
