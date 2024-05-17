88770996ce928097c8e35f551954da723c1a0785
kaifronsdal <kaif@stanford.edu>
2024-05-16 20:52:48 -0700

    merge

--- theoremsense/all_generate.sh
+++ theoremsense/all_generate.sh
@@ -22,23 +22,26 @@ export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=6,7
 # loop over all models, num_gpu pairs
 args=(
-    "meta-llama/Meta-Llama-3-70B-Instruct" 2
-    "mistralai/Mixtral-8x7B-Instruct-v0.1" 2
-    "meta-llama/Llama-2-70b-chat-hf" 2
-    "deepseek-ai/deepseek-llm-67b-chat" 2
+    "meta-llama/Meta-Llama-3-70B-Instruct" 4
+    "mistralai/Mixtral-8x7B-Instruct-v0.1" 4
+    "meta-llama/Llama-2-70b-chat-hf" 4
+    "deepseek-ai/deepseek-llm-67b-chat" 4
+
+    "EleutherAI/llemma_34b" 4
+    "mistralai/Mixtral-8x22B-v0.1" 4
+    "mistralai/Mixtral-8x22B-Instruct-v0.1" 4
+
     "mistralai/Mistral-7B-Instruct-v0.2" 1
     "meta-llama/Llama-2-7b-chat-hf" 1
     "meta-llama/Llama-2-13b-chat-hf" 1
     "meta-llama/Meta-Llama-3-8B-Instruct" 1
     "deepseek-ai/deepseek-math-7b-instruct" 1
+    "deepseek-ai/deepseek-math-7b-rl" 1
 # )
     "EleutherAI/llemma_7b" 1
     "GAIR/Abel-7B-002" 1
     "google/gemma-7b-it" 1
     "google/gemma-1.1-7b-it" 1
-#    "EleutherAI/llemma_34b" 4
-#    "mistralai/Mixtral-8x22B-v0.1" 4
-#    "mistralai/Mixtral-8x22B-Instruct-v0.1" 4
 )
 
 for ((i=0; i<${#args[@]}; i+=2))
