543c53527f1f1cbef6841b0e2ffa9e813a8556d9
Kai Fronsdal <kaif@ampere1.stanford.edu>
2024-06-06 01:30:15 -0700

    plots

--- theoremsense/metric_eval.py
+++ theoremsense/metric_eval.py
@@ -34,7 +34,7 @@ GPU_MAPPING = {
 }
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = GPU_MAPPING[args.index]
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"  # GPU_MAPPING[args.index]
 
 from pathlib import Path
 import pandas as pd
@@ -72,34 +72,34 @@ class BoxedMatch(Metric):
         return results  # [cols]
 
 
-# import torch
-#
-# torch.set_float32_matmul_precision('medium')
-#
-# from comet import download_model, load_from_checkpoint
-#
-#
-# class COMET(Metric):
-#     def __init__(self, model_name="Unbabel/XCOMET-XL", batch_size=32, gpus=1):
-#         super().__init__()
-#         self.model_name = model_name
-#         self.model_path = download_model(model_name)
-#         self.model = load_from_checkpoint(self.model_path)
-#
-#         self.batch_size = batch_size
-#         self.gpus = gpus
-#
-#     def process(self, results):
-#         data = [
-#             {
-#                 "src": str(row['problem']),
-#                 "mt": str(row['prediction']),
-#                 "ref": str(row['solution'])
-#             } for _, row in results.iterrows()
-#         ]
-#         model_output = self.model.predict(data, batch_size=self.batch_size, gpus=self.gpus)
-#         # results[self.model_name] = model_output
-#         return model_output
+import torch
+
+torch.set_float32_matmul_precision('medium')
+
+from comet import download_model, load_from_checkpoint
+
+
+class COMET(Metric):
+    def __init__(self, model_name="Unbabel/XCOMET-XL", batch_size=32, gpus=1):
+        super().__init__()
+        self.model_name = model_name
+        self.model_path = download_model(model_name)
+        self.model = load_from_checkpoint(self.model_path)
+
+        self.batch_size = batch_size
+        self.gpus = gpus
+
+    def process(self, results):
+        data = [
+            {
+                "src": str(row['problem']),
+                "mt": str(row['prediction']),
+                "ref": str(row['solution'])
+            } for _, row in results.iterrows()
+        ]
+        model_output = self.model.predict(data, batch_size=self.batch_size, gpus=self.gpus)
+        # results[self.model_name] = model_output
+        return model_output
 
 
 # comet = COMET()
@@ -178,19 +178,21 @@ from roscoe.score import Evaluator
 
 EMB_MODEL_SCORES_FILTERED = [e for e in EMB_MODEL_SCORES if e not in [FAITHFUL_WORD, REPETITION_WORD]]
 
+Override = 0
+
 USED_SCORES = {
     0: EMB_MODEL_SCORES_FILTERED,
     1: NLI_MODEL_SCORES,
     2: LANGUAGE_MODEL_SCORES,
     3: GRAMMAR_MODEL_SCORES
-}[args.index]
+}[Override]
 
 USED_SCORES_NAME = {
     0: 'EMB_MODEL_SCORES',
     1: 'NLI_MODEL_SCORES',
     2: 'LANGUAGE_MODEL_SCORES',
     3: 'GRAMMAR_MODEL_SCORES'
-}[args.index]
+}[Override]
 
 print(USED_SCORES)
 
@@ -244,98 +246,112 @@ class ROSCOE(Metric):
             return scores
 
 
-#
-# results = pd.concat([
-#     pd.read_json(save_path / f)
-#     for f in save_path.iterdir()
-#     if f.suffix == '.json'
-# ])
-#
-# results = results.query('method == "autoregressive"')
-#
-# comet = COMET()
-# print('COMET')
-# results = comet(results)
-#
-# temp_save_path = save_path / 'COMET'
-# # ensure exists
-# temp_save_path.mkdir(parents=True, exist_ok=True)
-# # results.to_json(temp_save_path)
-# import pickle
-#
-# with open(temp_save_path / 'comet_final.pkl', 'wb') as f:
-#     pickle.dump(results, f)
-
-# because scoring using roscoe takes so long, we will score in batches
-# and checkpoint the results
-temp_save_path = save_path / 'ROSCOE'
-# ensure exists
-temp_save_path.mkdir(parents=True, exist_ok=True)
-# group by dataset
-roscoe = ROSCOE()
-import pickle
-import numpy as np
-import json
-
-# print(f'total len: {len(results.groupby("dataset"))}')
-
-dataset_groups = ['Number_Theory_test', 'Counting_and_Probability_train', 'Algebra_test',
-                  'Geometry_test', 'Number_Theory_train', 'Prealgebra_test',
-                  'Intermediate_Algebra_train', 'Prealgebra_train', 'Precalculus_train',
-                  'Intermediate_Algebra_test', 'Counting_and_Probability_test',
-                  'Geometry_train', 'Algebra_train', 'Precalculus_test']
-
-# split amongst 3 runs
-num_runs = len(GPU_MAPPING)
-i = args.index
-datasets_for_this_run = dataset_groups[i::num_runs]
-print(f'Processing {datasets_for_this_run}')
-
-# load the results and combine them back into a single dataframe
 results = pd.concat([
     pd.read_json(save_path / f)
     for f in save_path.iterdir()
-    if f.suffix == '.json'  # and f.stem in datasets_for_this_run
+    if f.suffix == '.json'
 ])
 
-print(f'Loaded {len(results)} results')
-
-# results = results[results['dataset'].isin(datasets_for_this_run)]
 results = results.query('method == "autoregressive"')
+
+comet = COMET()
+print('COMET')
+output = comet(results)
+
+try:
+    results['COMET'] = output['score']
+except Exception as e:
+    print(e)
+    breakpoint()
+
+temp_save_path = save_path / 'COMET'
+# ensure exists
+temp_save_path.mkdir(parents=True, exist_ok=True)
+# results.to_json(temp_save_path)
+import pickle
+import json
+
+try:
+    with open(temp_save_path / 'comet_final2.json', 'w') as f:
+        json.dump(output, f)
+except Exception as e:
+    print(f'Error saving')
+    print(e)
+    breakpoint()
+
+# with open(temp_save_path / 'comet_final2.pkl', 'wb') as f:
+#     pickle.dump(output, f)
+
+# # because scoring using roscoe takes so long, we will score in batches
+# # and checkpoint the results
+# temp_save_path = save_path / 'ROSCOE'
+# # ensure exists
+# temp_save_path.mkdir(parents=True, exist_ok=True)
+# # group by dataset
+# roscoe = ROSCOE(transformer_model="princeton-nlp/sup-simcse-roberta-base")
+# import pickle
+# import numpy as np
+# import json
 #
-import cProfile
-import pstats
-
-profiler = cProfile.Profile()
-profiler.enable()
-
-for dataset, group in list(results.groupby('dataset')):
-    # sample 300 examples per model
-    np.random.seed(42)
-    question_ids = np.random.choice(group['i'].unique(), size=100, replace=False)
-    # breakpoint()
-    print(f'Number of questions: {len(question_ids)}')
-    group = group[group['i'].isin(question_ids)]
-    print(f'Processing {dataset} with {len(group)} examples and {len(group["model"].unique())} models')
-    # breakpoint()
-    output = roscoe(group.copy())
-
-    # group.to_json(temp_save_path / f'{dataset}.json')
-    # try:
-    #     output.to_json(temp_save_path / f'{dataset}.json')
-    # except Exception as e:
-    #     print(f'Error saving {dataset}')
-    #     print(e)
-    #     breakpoint()
-    try:
-        with open(temp_save_path / f'{dataset}.json', 'w') as f:
-            json.dump(output.to_dict('records'), f)
-    except Exception as e:
-        print(f'Error saving {dataset}')
-        print(e)
-        breakpoint()
-
-profiler.disable()
-
-# save the profile to a file
-profiler.dump_stats('profile.prof')
+# # print(f'total len: {len(results.groupby("dataset"))}')
+#
+# dataset_groups = ['Number_Theory_test', 'Counting_and_Probability_train', 'Algebra_test',
+#                   'Geometry_test', 'Number_Theory_train', 'Prealgebra_test',
+#                   'Intermediate_Algebra_train', 'Prealgebra_train', 'Precalculus_train',
+#                   'Intermediate_Algebra_test', 'Counting_and_Probability_test',
+#                   'Geometry_train', 'Algebra_train', 'Precalculus_test']
+#
+# # split amongst 3 runs
+# num_runs = len(GPU_MAPPING)
+# i = args.index
+# datasets_for_this_run = dataset_groups[i::num_runs]
+# print(f'Processing {datasets_for_this_run}')
+#
+# # load the results and combine them back into a single dataframe
+# results = pd.concat([
+#     pd.read_json(save_path / f)
+#     for f in save_path.iterdir()
+#     if f.suffix == '.json'  # and f.stem in datasets_for_this_run
+# ])
+#
+# print(f'Loaded {len(results)} results')
+#
+# # results = results[results['dataset'].isin(datasets_for_this_run)]
+# results = results.query('method == "autoregressive"')
+# #
+# # import cProfile
+# # import pstats
+# #
+# # profiler = cProfile.Profile()
+# # profiler.enable()
+#
+# for dataset, group in list(results.groupby('dataset')):
+#     # sample 300 examples per model
+#     # np.random.seed(42)
+#     # question_ids = np.random.choice(group['i'].unique(), size=100, replace=False)
+#     # # breakpoint()
+#     # print(f'Number of questions: {len(question_ids)}')
+#     # group = group[group['i'].isin(question_ids)]
+#     print(f'Processing {dataset} with {len(group)} examples and {len(group["model"].unique())} models')
+#     # breakpoint()
+#     output = roscoe(group.copy())
+#
+#     # group.to_json(temp_save_path / f'{dataset}.json')
+#     # try:
+#     #     output.to_json(temp_save_path / f'{dataset}.json')
+#     # except Exception as e:
+#     #     print(f'Error saving {dataset}')
+#     #     print(e)
+#     #     breakpoint()
+#     try:
+#         with open(temp_save_path / f'SUPSIMCE_MODEL_{USED_SCORES_NAME}_{dataset}.json', 'w') as f:
+#             json.dump(output.to_dict('records'), f)
+#     except Exception as e:
+#         print(f'Error saving {dataset}')
+#         print(e)
+#         breakpoint()
+#
+# # profiler.disable()
+# #
+# # # save the profile to a file
+# # profiler.dump_stats('profile.prof')
